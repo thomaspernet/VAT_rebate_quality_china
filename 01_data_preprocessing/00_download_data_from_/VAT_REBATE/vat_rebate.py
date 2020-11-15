cc57d4341806bdc8f2e7b29a46ff45bc9c9144b9
thomaspernet <t.pernetcoudrier@gmail.com>
2020-11-15 07:25:02 +0100

    Download vat spreadsheet

--- 01_data_preprocessing/00_download_data_from_/VAT_REBATE/vat_rebate.py
+++ 01_data_preprocessing/00_download_data_from_/VAT_REBATE/vat_rebate.py
@@ -17,3 +17,33 @@ auth = authorization_service.get_authorization(
 
 gd_auth = auth.authorization_drive()
 drive = connect_drive.drive_operations(gd_auth)
+
+spreadsheet_id = drive.find_file_id('base_hs6_VAT_2002_2012', to_print=False)
+
+### Load from spreadsheet
+vat = drive.upload_data_from_spreadsheet(
+    sheetID = spreadsheet_id,
+    sheetName = "base_hs6_VAT_2002_2012.csv",
+	 to_dataframe = True)
+
+
+### Move to S3
+from awsPy.aws_authorization import aws_connector
+from awsPy.aws_s3 import service_s3
+
+name_credential = 'thomas_vat_credentials.csv'
+region = 'eu-west-3'
+bucket = 'chinese-data'
+path_cred = "{0}/creds/{1}".format(parent_path, name_credential)
+
+con = aws_connector.aws_instantiate(credential = path_cred,
+                                       region = region)
+client= con.client_boto()
+s3 = service_s3.connect_S3(client = client,
+                      bucket = bucket, verbose = True)
+
+vat.to_csv('base_hs6_VAT_2002_2012.csv', index = False)
+s3.upload_file(file_to_upload = 'base_hs6_VAT_2002_2012.csv',
+destination_in_s3 = 'DATA/TAX_DATA/TRANSFORMED/VAT_REBATE')
+
+os.remove('base_hs6_VAT_2002_2012.csv')
