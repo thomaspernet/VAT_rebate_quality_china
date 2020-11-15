6829041a3736957b15797d602888e4ad94059be5
thomaspernet <t.pernetcoudrier@gmail.com>
2020-11-15 08:09:18 +0100

    Add log vat rebate tax

--- 01_data_preprocessing/00_download_data_from_/VAT_REBATE/vat_rebate.py
+++ 01_data_preprocessing/00_download_data_from_/VAT_REBATE/vat_rebate.py
@@ -1,7 +1,12 @@
+from awsPy.aws_s3 import service_s3
+from awsPy.aws_authorization import aws_connector
 import pandas as pd
 import numpy as np
 from pathlib import Path
-import os, re,  requests, json
+import os
+import re
+import requests
+import json
 from GoogleDrivePy.google_drive import connect_drive
 from GoogleDrivePy.google_authorization import authorization_service
 path = os.getcwd()
@@ -10,9 +15,9 @@ project = 'valid-pagoda-132423'
 
 
 auth = authorization_service.get_authorization(
-    path_credential_gcp = "{}/creds/service.json".format(parent_path),
-    path_credential_drive = "{}/creds".format(parent_path),
-    verbose = False#
+    path_credential_gcp="{}/creds/service.json".format(parent_path),
+    path_credential_drive="{}/creds".format(parent_path),
+    verbose=False
 )
 
 gd_auth = auth.authorization_drive()
@@ -20,30 +25,36 @@ drive = connect_drive.drive_operations(gd_auth)
 
 spreadsheet_id = drive.find_file_id('base_hs6_VAT_2002_2012', to_print=False)
 
-### Load from spreadsheet
-vat = drive.upload_data_from_spreadsheet(
-    sheetID = spreadsheet_id,
-    sheetName = "base_hs6_VAT_2002_2012.csv",
-	 to_dataframe = True)
+# Load from spreadsheet
+vat = (
+    drive.upload_data_from_spreadsheet(
+        sheetID=spreadsheet_id,
+        sheetName="base_hs6_VAT_2002_2012.csv",
+        to_dataframe=True)
+    .assign(
+        ln_tax_rebate=lambda x:
+        np.log(1 + pd.to_numeric(x['vat_reb_m'])
+               )
+    )
+
+)
 
 
-### Move to S3
-from awsPy.aws_authorization import aws_connector
-from awsPy.aws_s3 import service_s3
+# Move to S3
 
 name_credential = 'thomas_vat_credentials.csv'
 region = 'eu-west-3'
 bucket = 'chinese-data'
 path_cred = "{0}/creds/{1}".format(parent_path, name_credential)
 
-con = aws_connector.aws_instantiate(credential = path_cred,
-                                       region = region)
-client= con.client_boto()
-s3 = service_s3.connect_S3(client = client,
-                      bucket = bucket, verbose = True)
+con = aws_connector.aws_instantiate(credential=path_cred,
+                                    region=region)
+client = con.client_boto()
+s3 = service_s3.connect_S3(client=client,
+                           bucket=bucket, verbose=True)
 
-vat.to_csv('base_hs6_VAT_2002_2012.csv', index = False)
-s3.upload_file(file_to_upload = 'base_hs6_VAT_2002_2012.csv',
-destination_in_s3 = 'DATA/TAX_DATA/TRANSFORMED/VAT_REBATE')
+vat.to_csv('base_hs6_VAT_2002_2012.csv', index=False)
+s3.upload_file(file_to_upload='base_hs6_VAT_2002_2012.csv',
+               destination_in_s3='DATA/TAX_DATA/TRANSFORMED/VAT_REBATE')
 
 os.remove('base_hs6_VAT_2002_2012.csv')
